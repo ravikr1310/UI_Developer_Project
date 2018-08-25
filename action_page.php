8c3d5e7efd1dfd66748f5a9b91a03505811819ad
Ravi Kumar <ravikr1310@gmail.com>
2018-08-25 08:13:57 +0530

    merger

---
+++ action_page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!-- saved from url=(0041)https://www.w3schools.com/action_page.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Forms action page</title>
+<link rel="stylesheet" href="./action_page_files/w3.css">
+</head><body class="w3-container">
+<h1>Submitted Form Data</h1>
+<h2>Your input was received as:</h2>
+<div id="id" class="w3-container w3-large w3-border" style="word-wrap:break-word">
+	
+&nbsp;</div>
+
+<p>The server has processed your input and returned this answer.</p>
+
+<div class="w3-panel w3-pale-yellow w3-leftbar w3-border-yellow">
+<p><strong>Note:</strong> This tutorial will not teach you how servers are processing input.
+Processing input is explained in our <a href="https://www.w3schools.com/php/default.asp" target="_blank">PHP tutorial</a>.</p>
+</div>
+
+<script type="text/javascript">
+	// var obj = document.getElementById("id");
+	// obj.innerHTML = localStorage.textvalue;
+</script>
+</body></html>
